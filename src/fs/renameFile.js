060bc095fe9a654e27be419b639b74b625e71b5e
Aliaksandra <alekspekhota@gmail.com>
2023-04-23 17:33:12 +0200

    feat: add createFilePath function

--- src/fs/renameFile.js
+++ src/fs/renameFile.js
@@ -1,34 +1,23 @@
 import path from 'path';
 import fs from 'fs/promises';
-import { checkisAbsolutePath, checkIsExist } from '../utilities.js'; 
+import { createFilePath } from '../utilities.js'; 
 
 export const renameFile = async (workingDirectory, props) => {
   if (props.length < 2) {
-    console.log('Operation failed');
+    console.log('Input invalid');
     return;
   }
   const filePath = props[0];
   const newFileName = props[1];
 
-  const isAbsolute = checkisAbsolutePath(filePath);
-  let oldPath;
-
-  if (isAbsolute) {
-     oldPath = path.resolve(filePath);
-  } else {
-     oldPath = path.resolve(workingDirectory, filePath);
-  }
-
-  // await checkIsExist(oldPath);
+  const oldPath = createFilePath(workingDirectory, filePath);
 
   const newPath = path.resolve(path.dirname(oldPath), newFileName);
-
-  // await checkIsExist(newPath);
 
   try {
     await fs.rename(oldPath, newPath)
   } catch (error) {
-    console.log('Operation failed');
+    console.log('Operation failed: there is not such file');
   }
 
 } 
